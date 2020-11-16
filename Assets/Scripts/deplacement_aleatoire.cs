0122f3d34f057bc436ec319ccac892206e658ad9
Maxalaar <50994841+Maxalaar@users.noreply.github.com>
2020-11-16 15:11:22 +0100

    creation_spawn
    création du spawn

---
+++ Assets/Scripts/deplacement_aleatoire.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deplacement_aleatoire : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float movementSpeed = 0.25f;
+    public float rotationSpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0.0f, 0.0f, 0.0f) * rotationSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(1f, 0.0f, 1f);
+        transform.Translate(movement * movementSpeed);
+    }
+}
