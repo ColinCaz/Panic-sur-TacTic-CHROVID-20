d767eeebc3f584fbbee3174e06e4a3828656a638
wiyochi <mat.arquilliere@gmail.com>
2020-11-16 14:59:00 +0100

    rechargement + changement bruitage

--- Assets/Scripts/GunBehaviour.cs
+++ Assets/Scripts/GunBehaviour.cs
@@ -13,10 +13,20 @@ public class GunBehaviour : MonoBehaviour
     public float timeBetweenFire = 0.5f;
     private float timePassed = 0.0f;
 
+    public int maxLoad = 10;
+    public int load = 10;
+    public float reloadTime = 2.0f;
+    private float reloadTimePassed = 0.0f;
+    private bool reloading = false;
+
+    public AudioClip shotSound;
+    public AudioClip reloadSound;
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,23 +34,41 @@ public class GunBehaviour : MonoBehaviour
     {
         timePassed += Time.deltaTime;
 
-        if (Input.GetButton("Fire1") && timePassed > timeBetweenFire)
+        if (reloading)
         {
-            Fire();
-            timePassed = 0.0f;
+            reloadTimePassed += Time.deltaTime;
+            if (reloadTimePassed > reloadTime)
+            {
+                reloading = false;
+                load = maxLoad;
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Fire1") && timePassed > timeBetweenFire && load > 0)
+            {
+                Fire();
+                timePassed = 0.0f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.R) && load < maxLoad)
+            {
+                reloading = true;
+                reloadTimePassed = 0;
+                audioSource.PlayOneShot(reloadSound);
+            }
         }
     }
 
     private void Fire()
     {
-        GetComponent<AudioSource>().Play();
+        load--;
+        audioSource.PlayOneShot(shotSound);
 
         GameObject mask = Instantiate(maskPrefab);
-
         Physics.IgnoreCollision(mask.GetComponent<Collider>(), maskSpawn.parent.GetComponent<Collider>());
 
         mask.transform.position = maskSpawn.position;
-
         Vector3 rotation = mask.transform.rotation.eulerAngles;
         mask.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
         mask.GetComponent<Rigidbody>().AddForce(maskSpawn.forward * maskSpeed, ForceMode.Impulse);
