b02bf1fb81617f3f71c9866301187c37da99c6b8
Cazaubon <ccolin333@gmail.com>
2020-11-20 19:32:28 +0100

    Interfaces

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public Text nombreDeFragmentDeVirus;
+    public GameObject help;
+
+    public GameObject fragVirus;
+    public GameObject aide;
+    public GameObject quitter;
+    public GameObject solo;
+    public GameObject coop;
+    public GameObject skins;
+    public GameObject options;
+
+    public Text helpText;
+
+    void Start()
+    {
+        nombreDeFragmentDeVirus.text = PlayerPrefs.GetInt("FragVirus", 0).ToString();
+    }
+
+    public void Help()
+    {
+        help.SetActive(true);
+
+        fragVirus.SetActive(true);
+        aide.SetActive(false);
+        quitter.SetActive(true);
+        solo.SetActive(true);
+        coop.SetActive(true);
+        skins.SetActive(true);
+        options.SetActive(true);
+
+        helpText.text = "Ceci est le bouton sur lequel vous venez de cliquer : le bouton aide." + System.Environment.NewLine +
+            "Cliquez dessus si vous avez un doute sur l'utilité des boutons du menu principal." + System.Environment.NewLine +
+            "Pour quitter l'aide, vous pouvez appuyer sur le bouton \"FERMER L'AIDE\" ou sur la touche \"ECHAP\" à tout moment.";
+    }
+
+    public void Next()
+    {
+        if (!aide.activeInHierarchy)
+        {
+            aide.SetActive(true);
+            quitter.SetActive(false);
+            helpText.text = "Si vous souhaitez quitter le jeu," + System.Environment.NewLine +
+                "appuyez sur ce bouton.";
+        }
+        else if (!quitter.activeInHierarchy)
+        {
+            quitter.SetActive(true);
+            fragVirus.SetActive(false);
+            helpText.text = "Ce nombre représente les fragments de virus en votre possession." + System.Environment.NewLine +
+                "Les fragments de virus servent uniquement à acheter des skins," + System.Environment.NewLine +
+                "ils s'obtiennent à la fin de chaque ville en fonction de vos performances.";
+        }
+        else if (!fragVirus.activeInHierarchy)
+        {
+            fragVirus.SetActive(true);
+            solo.SetActive(false);
+            helpText.text = "Cliquez sur ce bouton pour lancer une partie en solo." + System.Environment.NewLine +
+                "La population TacTic compte sur vous pour les protéger !";
+        }
+        else if (!solo.activeInHierarchy)
+        {
+            solo.SetActive(true);
+            coop.SetActive(false);
+            helpText.text = "Cliquez sur ce bouton pour lancer une partie en multijoueur." + System.Environment.NewLine +
+                "Vous et vos amis allez devoir coopérer pour sauver la planète TacTic !";
+        }
+        else if (!coop.activeInHierarchy)
+        {
+            coop.SetActive(true);
+            skins.SetActive(false);
+            helpText.text = "Ce bouton vous permet d'accéder à la boutique des skins." + System.Environment.NewLine +
+                "Servez vous des fragments de virus que vous avez récoltés pour faire vos achats !";
+        }
+        else if (!skins.activeInHierarchy)
+        {
+            skins.SetActive(true);
+            options.SetActive(false);
+            helpText.text = "Pour accéder aux options du jeu, cliquez sur ce bouton." + System.Environment.NewLine +
+                "Les options sont aussi accessibles en jeu depuis le menu pause.";
+        }
+        else if (!options.activeInHierarchy)
+        {
+            options.SetActive(true);
+            helpText.text = "L'aide est terminée !" + System.Environment.NewLine +
+                "N'hésitez pas à revenir pour vous rafraîchir la mémoire.";
+        }
+        else
+        {
+            FinHelp();
+        }
+    }
+
+    public void FinHelp()
+    {
+        help.SetActive(false);
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape") && help.activeInHierarchy)
+        {
+            FinHelp();
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 0.25f;
     public float rotationSpeed = 5;
+    public float jumpHeight = 25000;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -19,6 +21,9 @@ public class PlayerController : MonoBehaviour
 
     public GameObject minimapCamera;
     public GameObject mapCamera;
+
+    public GameObject borderGun;
+    public GameObject borderGrenade;
 
     void Start()
     {
@@ -36,20 +41,39 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         movement = transform.TransformDirection(movement);
-        rb.MovePosition(transform.position + movement * movementSpeed);
-        Quaternion rotation = Quaternion.Euler(new Vector3((invertVertical ? 1 : -1) * Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0.0f) * rotationSpeed);
+        rb.MovePosition(transform.position + movement * movementSpeed * Time.timeScale);
+        Quaternion rotation = Quaternion.Euler(new Vector3((invertVertical ? 1 : -1) * Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0.0f) * rotationSpeed * Time.timeScale);
         rotation *= rb.rotation;
         rb.MoveRotation(rotation);
+        if ((Input.GetKeyDown("right shift") || Input.GetKeyDown("left shift") || Input.GetKeyDown("space")) && transform.position.y < 0.5f)
+        {
+            rb.AddForce(Vector3.up * jumpHeight * Time.timeScale);
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("m"))
+        if (Input.GetKeyDown("escape") && Time.timeScale != 0)
+        {
+            Time.timeScale = 0;
+            SceneManager.LoadScene("Pause", LoadSceneMode.Additive);
+        }
+        if (Input.GetKeyDown("1") || Input.GetKeyDown("[1]"))
+        {
+            borderGrenade.SetActive(false);
+            borderGun.SetActive(true);
+        }
+        if (Input.GetKeyDown("2") || Input.GetKeyDown("[2]"))
+        {
+            borderGun.SetActive(false);
+            borderGrenade.SetActive(true);
+        }
+        if ((Input.GetKeyDown("m") || Input.GetKeyDown("tab")) && Time.timeScale != 0)
         {
             Minimap.SetActive(false);
             Map.SetActive(true);
         }
-        if (Input.GetKeyUp("m"))
+        if ((Input.GetKeyUp("m") || Input.GetKeyUp("tab")) && Time.timeScale != 0)
         {
             Map.SetActive(false);
             Minimap.SetActive(true);
