7df0653fa32337093615918c8a494e8f418b79b5
Maxalaar <50994841+Maxalaar@users.noreply.github.com>
2020-11-30 11:08:59 +0100

    merge main

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 0.25f;
+    public float jumpHeight = 50000;
     public float rotationSpeed = 10;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    public int minXRotation = -15;
+    public int maxXRotation = 15;
+
+    public GameObject minimapCamera;
+    public GameObject mapCamera;
 
     void Start()
     {
+        Cursor.visible = false;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -27,9 +35,51 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         movement = transform.TransformDirection(movement);
-        rb.MovePosition(transform.position + movement * movementSpeed);
-        Quaternion rotation = Quaternion.Euler(new Vector3(0.0f, Input.GetAxis("Mouse X"), 0.0f) * rotationSpeed);
+        rb.AddForce(transform.position + movement * movementSpeed * Time.timeScale);
+        
+        Quaternion rotation = Quaternion.Euler(new Vector3(PlayerPrefs.GetInt("Inversion", -1) * Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0.0f) * rotationSpeed * Time.timeScale);
         rotation *= rb.rotation;
         rb.MoveRotation(rotation);
+        
+       // transform.Rotate(new Vector3(PlayerPrefs.GetInt("Inversion", -1) * Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * PlayerPrefs.GetInt("Sensibilite", 10) * Time.timeScale, Space.Self);
+
+        if ((Input.GetKeyDown("right shift") || Input.GetKeyDown("left shift") || Input.GetKeyDown("space")) && transform.position.y < 0.5f)
+        {
+            rb.AddForce(Vector3.up * jumpHeight * Time.timeScale);
+        }
+    }
+
+    void Update()
+    {
+        mapCamera.transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+        minimapCamera.transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+        Quaternion rotation = Quaternion.identity;
+        rotation.eulerAngles = new Vector3(90, transform.rotation.eulerAngles.y, 0);
+        mapCamera.transform.rotation = rotation;
+        minimapCamera.transform.rotation = rotation;
+
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        if (eulerRotation.x > 180)
+        {
+            if (eulerRotation.x < 360 + minXRotation)
+            {
+                rb.AddForce(new Vector3(-eulerRotation.x + minXRotation, 0, -eulerRotation.z));
+            }
+            else
+            {
+                rb.AddForce(new Vector3(0, 0, -eulerRotation.z));
+            }
+        }
+        else
+        {
+            if (eulerRotation.x > maxXRotation)
+            {
+                rb.AddForce(new Vector3(-eulerRotation.x + maxXRotation, 0, -eulerRotation.z));
+            }
+            else
+            {
+                rb.AddForce(new Vector3(0, 0, -eulerRotation.z));
+            }
+        }
     }
 }
