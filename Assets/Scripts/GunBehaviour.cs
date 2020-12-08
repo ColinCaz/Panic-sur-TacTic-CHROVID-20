a52b0abe911cd4196138fdecfdf759bbec74e50e
Cazaubon <ccolin333@gmail.com>
2020-12-08 20:01:10 +0100

    Intégration Gun et Grenade

--- Assets/Scripts/GunBehaviour.cs
+++ Assets/Scripts/GunBehaviour.cs
@@ -9,68 +9,41 @@ public class GunBehaviour : MonoBehaviour
 
     public float maskSpeed = 30;
     public float lifeTime = 3;
-
     public float timeBetweenFire = 0.5f;
-    private float timePassed = 0.0f;
-
-    public int maxLoad = 10;
-    public int load = 10;
-    public float reloadTime = 2.0f;
-    private float reloadTimePassed = 0.0f;
-    private bool reloading = false;
 
     public AudioClip shotSound;
-    public AudioClip reloadSound;
-    private AudioSource audioSource;
+    public AudioSource source;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
+    private bool firing = false;
 
     // Update is called once per frame
     void Update()
     {
-        timePassed += Time.deltaTime;
-
-        if (reloading)
+        if (Input.GetMouseButtonDown(0) && Time.timeScale != 0 && PlayerPrefs.GetInt("MunGun", 0) > 0 && !firing)
         {
-            reloadTimePassed += Time.deltaTime;
-            if (reloadTimePassed > reloadTime)
-            {
-                reloading = false;
-                load = maxLoad;
-            }
+            StartCoroutine(Firing());
         }
-        else
-        {
-            if (Input.GetButton("Fire1") && timePassed > timeBetweenFire && load > 0 && Time.timeScale!=0)
-            {
-                Fire();
-                timePassed = 0.0f;
-            }
+    }
 
-            if (Input.GetKeyDown(KeyCode.R) && load < maxLoad)
-            {
-                reloading = true;
-                reloadTimePassed = 0;
-                audioSource.PlayOneShot(reloadSound);
-            }
-        }
+    IEnumerator Firing()
+    {
+        firing = true;
+        Fire();
+        yield return new WaitForSeconds(timeBetweenFire);
+        firing = false;
     }
 
     private void Fire()
     {
-        load--;
-        audioSource.PlayOneShot(shotSound);
+        source.volume = (float)PlayerPrefs.GetInt("VolumeSons") / 100;
+        source.PlayOneShot(shotSound);
+        PlayerPrefs.SetInt("MunGun", PlayerPrefs.GetInt("MunGun", 0) - 1);
 
         GameObject mask = Instantiate(maskPrefab);
         Physics.IgnoreCollision(mask.GetComponent<Collider>(), maskSpawn.parent.GetComponent<Collider>());
-
         mask.transform.position = maskSpawn.position;
-        Vector3 rotation = mask.transform.rotation.eulerAngles;
-        mask.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+        //Vector3 rotation = mask.transform.rotation.eulerAngles;
+        //mask.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
         mask.GetComponent<Rigidbody>().AddForce(maskSpawn.forward * maskSpeed, ForceMode.Impulse);
 
         StartCoroutine(DestroyMaskAfterTime(mask, lifeTime));
@@ -79,7 +52,6 @@ public class GunBehaviour : MonoBehaviour
     private IEnumerator DestroyMaskAfterTime(GameObject mask, float delay)
     {
         yield return new WaitForSeconds(delay);
-
         Destroy(mask);
     }
 }
--- Assets/Scripts/ThrowGrenade.cs
+++ Assets/Scripts/ThrowGrenade.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class ThrowGrenade : MonoBehaviour
 {
-    public GameObject grenade;
+    public int grenadeSpeed = 10;
+    public Transform grenadeSpawn;
+    public GameObject grenadePrefab;
     public GameObject epaule;
+    public GameObject player;
+    public GameObject grenadeEnMain;
 
     bool throwing = false;
     int etape = 1;
@@ -13,36 +17,97 @@ public class ThrowGrenade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (PlayerPrefs.GetInt("MunGrenade", 0) > 0 && !throwing)
         {
-            throwing = true;
-            StartCoroutine(Stop());
+            grenadeEnMain.SetActive(true);
+            if (Input.GetMouseButtonDown(0) && Time.timeScale != 0)
+            {
+                throwing = true;
+            }
         }
-        if (throwing)
+        else if(!throwing)
         {
+            grenadeEnMain.SetActive(false);
+        }
+        if (throwing && Time.timeScale!=0)
+        {
+            float playerX;
+            if (player.transform.rotation.eulerAngles.x > 180)
+            {
+                playerX = player.transform.rotation.eulerAngles.x - 360;
+            }
+            else
+            {
+                playerX = player.transform.rotation.eulerAngles.x;
+            }
+            float epauleZ;
+            if (epaule.transform.rotation.eulerAngles.z > 180)
+            {
+                epauleZ = epaule.transform.rotation.eulerAngles.z - 360;
+            }
+            else
+            {
+                epauleZ = epaule.transform.rotation.eulerAngles.z;
+            }
             if (etape == 1)
             {
-                //epaule.transform.rotation = Quaternion.Slerp(epaule.transform.rotation, epaule.transform.rotation + Quaternion.Euler(0, 0, 30), Time.deltaTime * 7.5f);
+                if (epauleZ <  30 + playerX && epauleZ > -15 + playerX)
+                {
+                    epaule.transform.Rotate(0, 0, (30 + playerX) * Time.deltaTime * 5, Space.Self);
+                }
+                else
+                {
+                    etape++;
+                }
             }
             if (etape == 2)
             {
-                //epaule.transform.rotation = Quaternion.Slerp(epaule.transform.rotation, epaule.transform.rotation + Quaternion.Euler(0, 30, 0), Time.deltaTime * 7.5f);
+                if (epauleZ < 45 + playerX && epauleZ > -40 + playerX)
+                {
+                    epaule.transform.Rotate(0, 0, -(70 + playerX) * Time.deltaTime * 5, Space.Self);
+                }
+                else
+                {
+                    etape++;
+                    PlayerPrefs.SetInt("MunGrenade", PlayerPrefs.GetInt("MunGrenade", 0) - 1);
+                    Throw();
+                }
             }
             if (etape == 3)
             {
-                //epaule.transform.rotation = Quaternion.Slerp(epaule.transform.rotation, epaule.transform.rotation + Quaternion.Euler(0, 30, 0), Time.deltaTime * 7.5f);
+                if (epauleZ < 45 + playerX && epauleZ > -70 + playerX)
+                {
+                    epaule.transform.Rotate(0, 0, -(30 + playerX) * Time.deltaTime * 5, Space.Self);
+                }
+                else
+                {
+                    etape++;
+                }
+            }
+            if (etape == 4)
+            {
+                if (epauleZ < 0 + playerX && epauleZ > -85 + playerX)
+                {
+                    epaule.transform.Rotate(0, 0, (70 + playerX) * Time.deltaTime * 5, Space.Self);
+                }
+                else
+                {
+                    if (PlayerPrefs.GetInt("MunGrenade", 0) > 0)
+                    {
+                        grenadeEnMain.SetActive(true);
+                    }
+                    throwing = false;
+                    etape = 1;
+                }
             }
         }
     }
 
-    IEnumerator Stop()
+    private void Throw()
     {
-        yield return new WaitForSeconds(1);
-        etape++;
-        yield return new WaitForSeconds(1);
-        etape++;
-        yield return new WaitForSeconds(1);
-        throwing = false;
-        etape = 1;
+        GameObject grenade = Instantiate(grenadePrefab);
+        grenade.transform.position = grenadeSpawn.position;
+        grenade.GetComponent<Rigidbody>().AddRelativeForce(grenadeSpawn.forward * grenadeSpeed, ForceMode.Impulse);
+        grenadeEnMain.SetActive(false);
     }
 }
